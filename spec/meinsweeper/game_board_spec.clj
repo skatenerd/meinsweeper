0a207ab6dfb3ec6041f7e4902e4bb076b285d72f
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2013-08-18 11:13:21 -0500

    Generate underlying board data containing mines

--- spec/meinsweeper/game_board_spec.clj
+++ spec/meinsweeper/game_board_spec.clj
@@ -6,13 +6,15 @@
 
 
 (describe
-  "new game"
-  (it "makes a grid with specified rows cols and no mines"
+  "underlying data"
+  (it "has specified rows cols and no mines"
       (let [new-grid (new-grid 5 10 0)]
         (should= 5 (count new-grid))
         (should= 10 (count (first new-grid)))
         (should= (list vacant) (distinct (flatten new-grid)))
         ))
 
-
-          )
+  (it "has specified number of mines"
+      (let [new-grid (new-grid 10 10 5)]
+        (should= 5 (count (filter #(= mine %) (flatten new-grid))))
+        )))
--- src/meinsweeper/game_board.clj
+++ src/meinsweeper/game_board.clj
@@ -1,6 +1,21 @@
 (ns meinsweeper.game-board
-  (:require [meinsweeper.square-names :refer :all]))
+  (:require [clojure.core.logic :as lg]
+            [clojure.core.logic.fd :as fd]
+            [meinsweeper.square-names :refer :all]))
+
+(defn mine-positions [rows cols mines]
+  (let [row-positions (repeatedly mines #(rand-nth (range rows)))
+        col-positions (repeatedly mines #(rand-nth (range cols)))]
+    (map vector row-positions col-positions)
+    )
+  )
+
 
 (defn new-grid [rows cols mines]
-  (repeat rows (repeat cols vacant))
-  )
+  (let [mineless (vec (repeat rows (vec (repeat cols vacant))))
+        mine-positions (mine-positions rows cols mines)]
+    (reduce
+      #(update-in %1 %2 (fn [_] mine))
+      mineless
+      mine-positions)
+    ))
