7376b04ca53b5a3d2cc317505424454dde57ec0c
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2013-08-27 12:11:51 -0500

    extract-method for grid neighbors

--- src/meinsweeper/ai/grid.clj
+++ src/meinsweeper/ai/grid.clj
@@ -5,13 +5,21 @@
 (defn square-at [grid [row-idx col-idx]]
   ((grid row-idx) col-idx) )
 
+(defn on-grid [[row col] rows cols]
+  (lg/all
+    (fd/in row (fd/interval 0 (dec rows)))
+    (fd/in col (fd/interval 0 (dec cols)))))
+
+(defn close [[row-1 col-1] [row-2 col-2]]
+  (lg/all
+    (fd/in row-1 (fd/interval (- row-2 1) (+ row-2 1)))
+    (fd/in col-1 (fd/interval (- col-2 1) (+ col-2 1)))))
+
 (defn neighbors [rows cols square]
   (let [[square-row square-col] square]
     (set (lg/run* [row col]
-           (fd/in row (fd/interval 0 (dec rows)))
-           (fd/in col (fd/interval 0 (dec cols)))
-           (fd/in row (fd/interval (- square-row 1) (+ square-row 1)))
-           (fd/in col (fd/interval (- square-col 1) (+ square-col 1)))))))
+                  (on-grid [row col] rows cols)
+                  (close [row col] square)))))
 
 (defn rows-count [grid]
   (count grid))
